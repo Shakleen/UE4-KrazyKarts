0acae430649a3a8b6bf5c2561709f7da2ca52245
shakleenishfar <shakleenishfar@gmail.com>
2022-04-26 17:13:13 +0600

    Added rotation mechanism and air resistance

--- Source/KrazyKarts/GoKart.cpp
+++ Source/KrazyKarts/GoKart.cpp
@@ -4,14 +4,18 @@
 #include "GoKart.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/BoxComponent.h"
 
 // Sets default values
 AGoKart::AGoKart()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
+	SetRootComponent(BoxCollision);
+
 	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Skeletal Mesh"));
-	SetRootComponent(Mesh);
+	Mesh->SetupAttachment(BoxCollision);
 
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
 	SpringArm->TargetOffset = FVector(0.f, 0.f, 200.f);
@@ -42,12 +46,37 @@ void AGoKart::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
-	FVector Force = GetActorForwardVector() * Throttle * MaxDrivingForceInNeutons;
+	FVector Force = GetActorForwardVector() * Throttle * MaxDrivingForceInNeutons + GetAirResistance();
 	FVector Accelartion = Force / CarMassInKG;
 	VelocityMetersPerSecond = VelocityMetersPerSecond + Accelartion * DeltaTime;
+	ApplyRotation(DeltaTime);
+	UpdateLocationFromVelocity(DeltaTime);
+}
 
+void AGoKart::ApplyRotation(float DeltaTime)
+{
+	float RotationAngle = MaxRotationInDegreesPerSecond * DeltaTime * SteeringThrow;
+	FQuat RotationDelta(GetActorUpVector(), FMath::DegreesToRadians(RotationAngle));
+	AddActorWorldRotation(RotationDelta);
+	VelocityMetersPerSecond = RotationDelta.RotateVector(VelocityMetersPerSecond);
+}
+
+void AGoKart::UpdateLocationFromVelocity(float DeltaTime)
+{
 	FVector Translation = VelocityMetersPerSecond * 100 * DeltaTime;
-	AddActorWorldOffset(Translation);
+
+	FHitResult HitResult;
+	AddActorWorldOffset(Translation, true, &HitResult);
+
+	if (HitResult.IsValidBlockingHit())
+	{
+		VelocityMetersPerSecond = FVector::ZeroVector;
+	}
+}
+
+FVector AGoKart::GetAirResistance()
+{
+	return -VelocityMetersPerSecond.SizeSquared() * DragCoefficient * VelocityMetersPerSecond.GetSafeNormal();
 }
 
 // Called to bind functionality to input
@@ -56,10 +85,16 @@ void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::MoveRight);
 }
 
 void AGoKart::MoveForward(float AxisValue)
 {
 	Throttle = AxisValue;
+}
+
+void AGoKart::MoveRight(float AxisValue)
+{
+	SteeringThrow = AxisValue;
 }
 
--- Source/KrazyKarts/GoKart.h
+++ Source/KrazyKarts/GoKart.h
@@ -8,6 +8,7 @@
 
 class UCameraComponent;
 class USpringArmComponent;
+class UBoxComponent;
 
 UCLASS(Abstract, Blueprintable)
 class KRAZYKARTS_API AGoKart : public APawn
@@ -26,21 +27,30 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
 	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+
+	void ApplyRotation(float DeltaTime);
+	void UpdateLocationFromVelocity(float DeltaTime);
+	FVector GetAirResistance();
 
 private:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, Category = "Components")
 	USkeletalMeshComponent* Mesh;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, Category = "Components")
 	USpringArmComponent* SpringArm;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UCameraComponent* Camera;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* BoxCollision;
 
 	UPROPERTY(VisibleAnywhere, Category = "State variables")
 	FVector VelocityMetersPerSecond;
@@ -48,9 +58,18 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "State variables")
 	float Throttle = 0.f;
 
+	UPROPERTY(VisibleAnywhere, Category = "State variables")
+	float SteeringThrow = 0.f;
+
 	UPROPERTY(EditAnywhere, Category = "Configuration Variables")
 	float CarMassInKG = 1000.f;
 
 	UPROPERTY(EditAnywhere, Category = "Configuration Variables")
 	float MaxDrivingForceInNeutons = 10000.f;
+
+	UPROPERTY(EditAnywhere, Category = "Configuration Variables")
+	float MaxRotationInDegreesPerSecond = 90.f;
+
+	UPROPERTY(EditAnywhere, Category = "Configuration Variables")
+	float DragCoefficient = 16.f;
 };
